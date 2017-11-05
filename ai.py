cede5f72d2837ce632d8c739092973b787423b1b
alexeladas <alexeladas@gmail.com>
2017-11-05 11:09:51 -0500

    finding possible moves

--- ai.py
+++ ai.py
@@ -17,13 +17,35 @@ class AI:
         self.number_of_tokens = 22
         self.non_attacking_moves = 0
         self.possible_moves = []
+        self.evaluation = 0
+        self.future_board = []
+
+    def evaluate(self,opponent, board):
+        for i in range(board.LENGTH):
+            for j in range(board.WIDTH):
+                if board.board[i][j].token == "G":
+                    self.evaluation += 100 * (i+1) + 50 * (j + 1)
+                if board.board[i][j].token == "R":
+                    self.evaluation -= 100 * (i+1) + 50 * (j + 1)
+    def build_tree(self, board, ai):
+        tree = {}
+        self.future_board = board.board
+        for move in self.possible_moves:
+            self.future_board.move(move, ai)
+            if self.future_board.check_if_attacking_move(move, ai):
+                self.future_board.attack(move, ai)
+            tree[move] = self.future_board
+        pass
+
+    def minmax(self):
+        pass
 
     def find_possible_moves(self, board):
         for i in range(board.LENGTH):
             for j in range(board.WIDTH):
                 if board.board[i][j].token == " ":
                     if i == 0 and j == 0:
-                        if board.board[j][j].cell_colour == "w":
+                        if board.board[i][j].cell_colour == "w":
                             if board.board[i+1][j].token == self.colour:
                                 self.possible_moves.append(self.MAP[i+1]+str(j+1)+" "+self.MAP[i]+str(j+1))
                             if board.board[i][j+1].token == self.colour:
